21da146be65283beba3fffcd120ee32e24cfbfa2
Kayla Fitzsimmons <kfitzsimmons11@gmail.com>
2016-02-04 17:38:43 -0800

    add php swagger code gen

---
+++ silex-PHP-server-4/SwaggerServer/index.php
@@ -0,0 +1,264 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Silex\Application;
+
+$app = new Silex\Application();
+
+    
+        
+            
+
+$app->PUT('/category/add', function(Application $app, Request $request, $category_name) {
+            
+            
+            return new Response('How about implementing categoryAddPut as a PUT method ?');
+            });
+
+            
+
+$app->GET('/category/all', function(Application $app, Request $request) {
+            
+            
+            return new Response('How about implementing categoryAllGet as a GET method ?');
+            });
+
+            
+
+$app->GET('/category/repair', function(Application $app, Request $request) {
+            
+            
+            return new Response('How about implementing categoryRepairGet as a GET method ?');
+            });
+
+            
+
+$app->GET('/category/reuse', function(Application $app, Request $request) {
+            
+            
+            return new Response('How about implementing categoryReuseGet as a GET method ?');
+            });
+
+            
+
+$app->PUT('/category/{category_id}', function(Application $app, Request $request, $category_id) {
+            
+            
+            return new Response('How about implementing categoryCategoryIdPut as a PUT method ?');
+            });
+
+            
+
+$app->DELETE('/category/{category_id}', function(Application $app, Request $request, $category_id) {
+            
+            
+            return new Response('How about implementing categoryCategoryIdDelete as a DELETE method ?');
+            });
+
+            
+        
+    
+        
+            
+
+$app->GET('/subcategory', function(Application $app, Request $request) {
+            
+            
+            return new Response('How about implementing subcategoryGet as a GET method ?');
+            });
+
+            
+
+$app->PUT('/subcategory/add', function(Application $app, Request $request, $subcategory_name) {
+            
+            
+            return new Response('How about implementing subcategoryAddPut as a PUT method ?');
+            });
+
+            
+
+$app->POST('/subcategory/{subcategory}/edit', function(Application $app, Request $request, $subcategory_id) {
+            
+            
+            return new Response('How about implementing subcategorySubcategoryEditPost as a POST method ?');
+            });
+
+            
+
+$app->DELETE('/subcategory/{subcategory}/edit', function(Application $app, Request $request, $subcategory_id) {
+            
+            
+            return new Response('How about implementing subcategorySubcategoryEditDelete as a DELETE method ?');
+            });
+
+            
+
+$app->GET('/subcategory/{subcategory}/repair', function(Application $app, Request $request) {
+            
+            
+            return new Response('How about implementing subcategorySubcategoryRepairGet as a GET method ?');
+            });
+
+            
+
+$app->GET('/subcategory/{subcategory}/reuse', function(Application $app, Request $request) {
+            
+            
+            return new Response('How about implementing subcategorySubcategoryReuseGet as a GET method ?');
+            });
+
+            
+        
+    
+        
+            
+
+$app->GET('/business/reuse', function(Application $app, Request $request) {
+            
+            
+            return new Response('How about implementing businessReuseGet as a GET method ?');
+            });
+
+            
+
+$app->GET('/category/reuse', function(Application $app, Request $request) {
+            
+            
+            return new Response('How about implementing categoryReuseGet as a GET method ?');
+            });
+
+            
+
+$app->GET('/subcategory/{subcategory}/reuse', function(Application $app, Request $request) {
+            
+            
+            return new Response('How about implementing subcategorySubcategoryReuseGet as a GET method ?');
+            });
+
+            
+        
+    
+        
+            
+
+$app->GET('/business/repair', function(Application $app, Request $request) {
+            
+            
+            return new Response('How about implementing businessRepairGet as a GET method ?');
+            });
+
+            
+
+$app->GET('/category/repair', function(Application $app, Request $request) {
+            
+            
+            return new Response('How about implementing categoryRepairGet as a GET method ?');
+            });
+
+            
+
+$app->GET('/subcategory/{subcategory}/repair', function(Application $app, Request $request) {
+            
+            
+            return new Response('How about implementing subcategorySubcategoryRepairGet as a GET method ?');
+            });
+
+            
+        
+    
+        
+            
+
+$app->PUT('/business/add', function(Application $app, Request $request) {
+            
+            
+            return new Response('How about implementing businessAddPut as a PUT method ?');
+            });
+
+            
+
+$app->GET('/business/all', function(Application $app, Request $request) {
+            
+            
+            return new Response('How about implementing businessAllGet as a GET method ?');
+            });
+
+            
+
+$app->GET('/business/repair', function(Application $app, Request $request) {
+            
+            
+            return new Response('How about implementing businessRepairGet as a GET method ?');
+            });
+
+            
+
+$app->GET('/business/repair/{subcategory}', function(Application $app, Request $request, $subcategory) {
+            
+            
+            return new Response('How about implementing businessRepairSubcategoryGet as a GET method ?');
+            });
+
+            
+
+$app->GET('/business/repair/{subcategory}/{geolocation}', function(Application $app, Request $request, $subcategory, $geolocation) {
+            
+            
+            return new Response('How about implementing businessRepairSubcategoryGeolocationGet as a GET method ?');
+            });
+
+            
+
+$app->GET('/business/reuse', function(Application $app, Request $request) {
+            
+            
+            return new Response('How about implementing businessReuseGet as a GET method ?');
+            });
+
+            
+
+$app->GET('/business/reuse/{subcategory}', function(Application $app, Request $request, $subcategory) {
+            
+            
+            return new Response('How about implementing businessReuseSubcategoryGet as a GET method ?');
+            });
+
+            
+
+$app->POST('/business/reuse/{subcategory}/{geolocation}', function(Application $app, Request $request, $subcategory_id, $geolocation) {
+            
+            
+            return new Response('How about implementing businessReuseSubcategoryGeolocationPost as a POST method ?');
+            });
+
+            
+
+$app->GET('/business/{business_id}', function(Application $app, Request $request, $business_id) {
+            
+            
+            return new Response('How about implementing businessBusinessIdGet as a GET method ?');
+            });
+
+            
+
+$app->POST('/business/{business_id}/edit', function(Application $app, Request $request, $user_id, $business_id) {
+            
+            
+            return new Response('How about implementing businessBusinessIdEditPost as a POST method ?');
+            });
+
+            
+
+$app->DELETE('/business/{business_id}/edit', function(Application $app, Request $request, $business_id) {
+            
+            
+            return new Response('How about implementing businessBusinessIdEditDelete as a DELETE method ?');
+            });
+
+            
+        
+    
+
+$app->run();
