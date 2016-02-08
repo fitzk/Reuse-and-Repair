765d7cebe622e4e732e3ff3a40f0a870f44296ed
root <root@ip-172-31-6-176.us-west-2.compute.internal>
2016-02-08 18:09:46 +0000

    add database access class

---
+++ server/Database.php
@@ -0,0 +1,21 @@
+/**
+ * Created by PhpStorm.
+ * User: kaylafitzsimmons
+ * Date: 2/4/16
+ * Time: 8:53 PM
+ */
+class Database
+{
+
+    private $user;
+    private $pass;
+    private $dbname;
+    private $host;
+    private $dbh;
+
+    
+    Database($user, $pass, $dbname, $host){
+	
+    }
+
+}
