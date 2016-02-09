b29484fff8a46d51367dcbd475c3e2be5aef5679
Philip Tan <tanp@onid.oregonstate.edu>
2016-02-09 01:42:28 -0800

    Changes to modals, formatting, login modal, general tweaks and updates

--- AdminUI/js/materialize.js
+++ AdminUI/js/materialize.js
@@ -382,31 +382,75 @@ if ($) {
 		    }
 		});
 
-		//JS FOR MODAL TRIGGER
+		//JS FOR ELEMENT INITIALIZATION
 		$(document).ready(function(){
 			// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
 
-			//DO BELOW FOR ADMINISTRATOR LOGIN BUTTON
+			//------------------------MODAL INITIALIZATIONS------------------------//
+			//--------------MODAL CODE FOR ADMIN LOGIN--------------//
+			//Initialize login button at top right corner
 			$('.loginbutton').leanModal({
 		      dismissible: true, // Modal can be dismissed by clicking outside of the modal
 		      opacity: .5, // Opacity of modal background
 		      in_duration: 300, // Transition in duration
 		      out_duration: 200, // Transition out duration
-		      //ready: function() { alert('Ready'); }, // Callback for Modal open
-		      //complete: function() { alert('Closed'); } // Callback for Modal close
 		    });
-		    //DO BELOW IF LOGIN IS SUCCESSFUL
+
+		    //This controls the login enter button, DO ONLY IF LOGIN IS SUCCESSFUL
 			$('.loginenter').leanModal({
-		      dismissible: true, // Modal can be dismissed by clicking outside of the modal
-		      opacity: 0, // Opacity of modal background
-		      in_duration: 800, // Transition in duration
-		      out_duration: 1000,//700, // Transition out duration
-		      //ready: function() { alert('Ready'); }, // Callback for Modal open
-		      //complete: function() { alert('Closed'); } // Callback for Modal close
+			  dismissible: false,
+		      in_duration: 800, 
+		      opacity: 0,
 		    });
+		    //Successful login response, delay then fade out and close modal if user doesn't dismiss
 			$('.loginenter').click(function() {
-				$('.modal.loginsuccess').delay(5000).fadeOut(500).closeModal();
+				$('.modal.loginsuccess').delay(6000).fadeOut(1000).closeModal();
 			});
+			//Close modal if dismiss is clicked
+			$('.modal.loginsuccess .dismissit').click(function() {			
+				$('.modal.loginsuccess').closeModal({
+			      out_duration: 800,
+				});
+			});
+
+			//--------------MODAL CODE FOR ADDING BUSINESS--------------//
+			$('.addbusiness').leanModal({
+		      dismissible: false,
+		      opacity: .5,
+		      in_duration: 300,
+		      out_duration: 300,
+		    });
+		    //Close add business modal form with click of X icon
+			$('.modal.businessmodal .modalclosex').click(function() {
+				$('.modal.businessmodal').closeModal({
+			      out_duration: 200,
+				});
+			});
+		    //This controls the submit button, DO ONLY IF SUBMIT IS SUCCESSFUL
+			$('.addsubmit').leanModal({
+			  dismissible: false,
+		      in_duration: 800, 
+		      opacity: 0,
+		    });
+		    //Successful submit response, delay then fade out and close modal if user doesn't dismiss
+			$('.addsubmit').click(function() {
+				$('.modal.addbussuccess').delay(6000).fadeOut(1000).closeModal();
+			});
+			//Close modal if dismiss is clicked
+			$('.modal.addbussuccess .dismissit').click(function() {			
+				$('.modal.addbussuccess').closeModal({
+			      out_duration: 800,
+				});
+			});
+
+			//--------------MODAL CODE FOR ADDING CATEGORY--------------//
+
+			//--------------MODAL CODE FOR ADDING ADMIN--------------//
+
+
+
+			//------------------------SELECT MENU INITIALIZATIONS------------------------//
+			$('.typeselect').material_select();
 		});
 
       // Accordion Open
