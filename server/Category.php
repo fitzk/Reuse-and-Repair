708b2236878dd6a4288dd9f6ddda9036bdb1146b
fitzsimk <kfitzsimmons11@gmail.com>
2016-02-09 17:56:35 -0800

    add getters and setters

--- server/Category.php
+++ server/Category.php
@@ -10,6 +10,48 @@ class Category
 {
     private $id;
    private $name;
-   
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Category constructor.
+     * @param $id
+     */
+    public function __construct($id,$name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
 
 }
