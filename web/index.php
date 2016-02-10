1684f7fa86c68057f73302bc0bbea57ec4659b8e
fitzsimk <kfitzsimmons11@gmail.com>
2016-02-09 16:39:17 -0800

    add dependency for server folder contents

--- web/index.php
+++ web/index.php
@@ -1,10 +1,12 @@
 <?php
 require_once __DIR__ .'/../vendor/autoload.php';
 
+require '../server/*';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Silex\Application;
+
 
 $app = new Silex\Application();
 
