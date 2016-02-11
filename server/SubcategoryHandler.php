c107ab52a70cc130caf0917f97221e31a6ec5c4f
tiegsb <briantiegs@gmail.com>
2016-02-10 20:11:16 -0500

    Update SubcategoryHandler.php

--- server/SubcategoryHandler.php
+++ server/SubcategoryHandler.php
@@ -6,6 +6,11 @@
  * Date: 2/4/16
  * Time: 8:49 PM
  */
+ 
+include_once('Database.php');
+include_once('Subcategory.php');
+include_once('Handler.php');
+
 class SubcategoryHandler extends Handler
 {
 
@@ -14,18 +19,48 @@ class SubcategoryHandler extends Handler
      */
     public function getAll()
     {
-        // TODO: Implement getAll() method.
+        $sql = "SELECT DISTINCT(reuse_and_repair_db.Subcategory.subcategory_name) FROM reuse_and_repair_db.Business INNER JOIN reuse_and_repair_db.Business_Subcategory ON reuse_and_repair_db.Business.business_id = reuse_and_repair_db.Business_Subcategory.business_id
+        	INNER JOIN reuse_and_repair_db.Subcategory ON reuse_and_repair_db.Business_Subcategory.subcategory_name = reuse_and_repair_db.Subcategory.subcategory_name
+        	ORDER BY reuse_and_repair_db.Subcategory.subcategory_name;";
+        $prepared = $this->db->link->prepare($sql);
+        $success = $prepared->execute();
+        $all = $prepared->fetchAll();
+        foreach ($all as $row) {
+          $subcategory = new Subcategory($row['subcategory_name'],$row['subcategory_name']);
+          $this->results[]= $subcategory->jsonSerialize();
+        }
+        return $this->getJSON();
     }
 
     /**
      * @param $id
      * @return string
      */
-    public function get($id)
+    public function getByCategory($category)
     {
-        // TODO: Implement get() method.
+        $sql = "SELECT DISTINCT(reuse_and_repair_db.Subcategory.subcategory_name) FROM reuse_and_repair_db.Business INNER JOIN reuse_and_repair_db.Business_Subcategory ON reuse_and_repair_db.Business.business_id = reuse_and_repair_db.Business_Subcategory.business_id
+        	INNER JOIN reuse_and_repair_db.Subcategory ON reuse_and_repair_db.Business_Subcategory.subcategory_name = reuse_and_repair_db.Subcategory.subcategory_name
+        	WHERE reuse_and_repair_db.Business.category_name = ?
+        	ORDER BY reuse_and_repair_db.Subcategory.subcategory_name;";
+        $prepared = $this->db->link->prepare($sql);
+        $prepared->bindParam(1, $category);
+        $success = $prepared->execute();
+        $all = $prepared->fetchAll();
+        foreach ($all as $row) {
+          $subcategory = new Subcategory($row['subcategory_name'],$row['subcategory_name']);
+          $this->results[]= $subcategory->jsonSerialize();
+        }
+        return $this->getJSON();
     }
 
+     /**
+     * @param $id
+     * @return string
+     */
+    public function get($id)
+    {
+        // TODO
+    }
     /**
      * @param $id
      * @return string
@@ -38,8 +73,9 @@ class SubcategoryHandler extends Handler
     /**
      * @return string
      */
-    public function update($id)
+    public function update($object)
     {
+        return json_encode($object);
         // TODO: Implement update() method.
     }
 
