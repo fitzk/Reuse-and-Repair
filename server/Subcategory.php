b91325e4df1ef27fb79dcacdb71ddf98f919bef8
tiegsb <briantiegs@gmail.com>
2016-02-10 20:09:49 -0500

    Update Subcategory.php

--- server/Subcategory.php
+++ server/Subcategory.php
@@ -9,5 +9,49 @@
 
 class Subcategory implements JsonSerializable
 {
-
+    private $id;
+    private $name;
+    public function jsonSerialize() {
+      return [ 
+        'id' => $this->id,
+        'name' => $this->name
+      ];
+    }
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+      return $this->id;
+    }
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    /**
+     * Category constructor.
+     * @param $id
+     */
+    public function __construct($id,$name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
 }
