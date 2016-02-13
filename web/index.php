27f2b5e1ec781677a63f9d6fcfeb70a0dbbe61c8
tiegsb <briantiegs@gmail.com>
2016-02-13 17:43:55 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -16,48 +16,36 @@ $app['debug'] = true;
 
 // Request::setTrustedProxies(array($ip));
 
-// Create
 $app->GET('/', function (Application $app, Request $request) {
 
-    return new Response("<p>All good</p>!", 200);
+    return new Response("<p>All good!</p>", 200);
 });
+
+
 // Category Routes
 // All categories
-// Create
-$app->POST('/newcategory', function (Application $app, Request $request) {
+$app->GET('/categories', function (Application $app, Request $request) {
+    
+    $handler = New CategoryHandler();
+    $result = $handler->getAll();
+    
+    return new Response($result, 200);
+});
+
+// Create category
+$app->PUT('/categories', function (Application $app, Request $request) {
 
     return new Response('How about implementing categoryCategoryIdPut as a POST method ?');
 });
 
-// Read All
-$app->GET('/categories', function (Application $app, Request $request) {
-    $handler = New CategoryHandler();
-    $result = $handler->getAll();
-    
-     return new Response($result, 200);
-});
-// Returns all businesses
-$app->GET('/businesses', function (Application $app, Request $request) {
-    $handler = New BusinessHandler();
-    $result = $handler->getAll();
-
-    return new Response($result, 200);
-
-    //  return new Response('How about implementing businessReuseGet as a GET method ?');
-});
-
-$app->GET('/categories/{category}', function (Application $app, Request $request) {
-    return new Response('How about implementing categoryAllGet as a GET method ?');
-});
-
-// update
-$app->PUT('/categories/{category}', function (Application $app, Request $request) {
+// Update category
+$app->POST('/categories/{category}', function (Application $app, Request $request) {
 
 
     return new Response('How about implementing categoryCategoryIdDelete as a DELETE method ?');
 });
 
-// destroy
+// Destroy category
 $app->DELETE('/categories/{category}', function (Application $app, Request $request) {
 
 
@@ -65,8 +53,8 @@ $app->DELETE('/categories/{category}', function (Application $app, Request $requ
 });
 
 
-// subcategory routes
-// Returns all subcategories
+// Subcategory routes
+// All subcategories
 $app->GET('/subcategories', function (Application $app, Request $request) {
     
     $handler = New SubcategoryHandler();
@@ -75,6 +63,7 @@ $app->GET('/subcategories', function (Application $app, Request $request) {
     return new Response($result, 200);
 });
 
+// Get subcategories by categories
 $app->GET('{category}/subcategories', function (Application $app, Request $request, $category) {
     
     $handler = New SubcategoryHandler();
@@ -83,69 +72,96 @@ $app->GET('{category}/subcategories', function (Application $app, Request $reque
     return new Response($result, 200);
 });
 
-$app->PUT('/subcategories', function (Application $app, Request $request, $subcategory_name) {
-
-
-    return new Response('How about implementing subcategoryAddPut as a PUT method ?');
+// Create subcategory
+$app->PUT('/subcategories', function (Application $app, Request $request) {
+    
+    //authenticate();
+    
+    $handler = New SubcategoryHandler();
+    $result = $handler->add($request->get('subcategory_name'));
+    
+    return new Response($result[0], $result[1]);
 });
 
-$app->POST('/subcategories/{subcategory}', function (Application $app, Request $request, $subcategory_id) {
+// Update subcategory
+$app->POST('/subcategories/{subcategory}', function (Application $app, Request $request, $subcategory) {
+    
+    //authenticate();
 
+    $object = array(
+      'subcategory' => $subcategory,
+      'new_subcategory' => $request->get('subcategory_name')
+    );
+    
+    $handler = New SubcategoryHandler();
+    $result = $handler->update($object);
+    
+    return new Response($result[0], $result[1]);
+});
 
-    return new Response('How about implementing subcategorySubcategoryEditPost as a POST method ?');
+// Delete subcategory
+$app->DELETE('/subcategories/{subcategory}', function (Application $app, Request $request, $subcategory) {
+
+    //authenticate();
+    
+    $handler = New SubcategoryHandler();
+    $result = $handler->delete($subcategory);
+    
+    return new Response($result[0], $result[1]);
 });
 
 
-$app->DELETE('/subcategories/{subcategory}', function (Application $app, Request $request, $subcategory_id) {
+// Business routes
+// All businesses
+$app->GET('/businesses', function (Application $app, Request $request) {
 
+    $handler = New BusinessHandler();
+    $result = $handler->getAll();
 
-    return new Response('How about implementing subcategorySubcategoryEditDelete as a DELETE method ?');
+    return new Response($result, 200);
 });
 
-
+// Get businesses by category
 $app->GET('/businesses/{category}', function (Application $app, Request $request) {
 
 
     return new Response('How about implementing businessRepairGet as a GET method ?');
 });
 
-
+// Create business
 $app->PUT('/businesses', function (Application $app, Request $request) {
 
 
     return new Response('How about implementing businessAddPut as a PUT method ?');
 });
 
-
-// optional geolocation in body
-$app->GET('/businesses/{category}/{subcategory}', function (Application $app, Request $request, $subcategory) {
-
-
-    return new Response('How about implementing businessRepairSubcategoryGet as a GET method ?');
-});
-
-
-
-
-
+// Get business by id
 $app->GET('/businesses/{business_id}', function (Application $app, Request $request, $business_id) {
 
 
     return new Response('How about implementing businessBusinessIdGet as a GET method ?');
 });
 
-
-$app->POST('/business', function (Application $app, Request $request, $user_id, $business_id) {
+// Update business
+$app->POST('/business/{business_id}', function (Application $app, Request $request, $user_id, $business_id) {
 
 
     return new Response('How about implementing businessBusinessIdEditPost as a POST method ?');
 });
 
 
-$app->DELETE('/business/{business_id}/edit', function (Application $app, Request $request, $business_id) {
+// Destroy business
+$app->DELETE('/business/{business_id}', function (Application $app, Request $request, $business_id) {
 
 
     return new Response('How about implementing businessBusinessIdEditDelete as a DELETE method ?');
+});
+
+// optional geolocation in body
+$app->GET('/businesses/{category}/{subcategory}', function (Application $app, Request $request, $subcategory) {
+
+
+    return new Response('How about implementing businessRepairSubcategoryGet as a GET method ?');
 });
 
 
