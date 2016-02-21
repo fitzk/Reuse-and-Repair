b6abb7c47c3ada2bc63f1d41a8926f8c493bf8de
Philip Tan <tanp@onid.oregonstate.edu>
2016-02-21 02:57:19 -0800

    Removing link tag

--- AdminUI/js/materialize.js
+++ AdminUI/js/materialize.js
@@ -361,11 +361,11 @@ if ($) {
 									if (value.website == value.website) {
 										console.log(value.website);									
 									}
-									$(".allbizlist").append("<a><tr>" + "<td>" + value.name + "</td>" + "<td>" + 
+									$(".allbizlist").append("<tr>" + "<td>" + value.name + "</td>" + "<td>" + 
 										value.address.street_number + " " + value.address.street_name + "<br>" + value.address.city + " " + value.address.state + 
 										", " + value.address.zip + "</td>" + "<td>" + "N/A" + "</td>" + "<td><a href=" + value.website + " target='_blank'>" + value.website + 
 										"</a></td>" + "<td>" + value.category + "</td>" + 
-										"<td><i class='material-icons arrowtate'>keyboard_arrow_right</i></td>" + "</tr></a>");
+										"<td><i class='material-icons arrowtate'>keyboard_arrow_right</i></td>" + "</tr>");
 								});
 							})
 
