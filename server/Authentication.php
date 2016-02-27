11eb85e5d84637d7b126e8f0760574bdcc28247d
tiegsb <briantiegs@gmail.com>
2016-02-27 14:57:08 -0500

    Update files

--- server/Authentication.php
+++ server/Authentication.php
@@ -53,4 +53,26 @@ function authenticate($access_level, $admin_id)
 	return false;
 }
 
+function getAdminIdByLogin()
+{
+  $username = $_SERVER['PHP_AUTH_USER'];
+  $password = $_SERVER['PHP_AUTH_PW'];
 
+  if($username == null || $password == null)
+    return false;
+
+  $db = new Database();
+  
+  $sql = "SELECT * FROM reuse_and_repair_db.Admin
+        WHERE reuse_and_repair_db.Admin.username = ? AND reuse_and_repair_db.Admin.password = ?;";
+  $prepared = $db->link->prepare($sql);
+  $prepared->bindParam(1, $username);
+  $prepared->bindParam(2, $password);
+  $success = $prepared->execute();
+  $result = $prepared->fetchAll();
+  
+  if($prepared->rowCount() == 0)
+    return -1;
+  
+  return $result[0]['fk_role_id'];
+}
